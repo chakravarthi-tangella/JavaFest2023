ee1c6a3f93539e9e3f96b43be313bf078b13b202
chakravarthi-tangella <chakravarthi.tangella@gmail.com>
2023-06-17 20:40:32 +0530

    adding Day 2 assignments

---
+++ StudentProgram/Student.java
@@ -0,0 +1,16 @@
+public class Student
+{
+	public static void main(String... args)
+	{
+		int marks1 = 50;
+		int marks2 = 60;
+		int marks3 = 70;
+
+		int totalMarks = marks1+marks2+marks3;
+
+		int averageMarks = totalMarks/3;
+
+		System.out.println("Total Marks of the student are : " + totalMarks);
+		System.out.println("Average Marks of the student are : " + averageMarks);
+	}
+}
